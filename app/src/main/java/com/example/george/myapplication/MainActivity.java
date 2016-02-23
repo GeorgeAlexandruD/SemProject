6ed39f015cb5fc819455b6e80ab6e6dbb1545d51
galmaverick <draganoiu.george@gmail.com>
2016-02-23 15:15:08 +0100

    asd

--- app/src/main/java/com/example/george/myapplication/MainActivity.java
+++ app/src/main/java/com/example/george/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     //lll
     //Teo change
+    //asd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
